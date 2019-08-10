d3dc72190697e866cd4ed4e6894a917e1c48a195
Oliver Smith <oliver@kfs.org>
2019-08-10 14:27:33 -0700

    Made tinker common

--- etc/Powershell.rc.d/Tinker.rc.ps1
+++ etc/Powershell.rc.d/Tinker.rc.ps1
@@ -1,20 +1,50 @@
-# Switch to one of my "tinkering" projects.
+# 'tinker' existed in d:\dev originally until I started having
+# a local 'github' folder, so although this file is focused on
+# 'tinker' it is now also kind of a natural home for my github
+# alias too.
 
-$script:DEFAULT_BASE = [System.Environment]::GetEnvironmentVariable("TINKER_BASE")
-if (! $script:DEFAULT_BASE) {
-  $script:DEFAULT_BASE = "D:\Dev\Tinker"
+if (!$env:DEV_PATH) {
+  if ((Test-Path D:) -And (Test-Path D:/Dev)) {
+    $env:DEV_PATH = Join-Path D: Dev
+  } else {
+    $env:DEV_PATH = Join-Path $HOME Development
+  }
 }
+$env:GITHUB_PATH = Join-Path $env:DEV_PATH github.com
+ 
+$env:TINKER_PATH = Join-Path $env:GITHUB_PATH kfsone/tinker
 
-Function Tinker
-{
-  [CmdletBinding(SupportsShouldProcess=$true)]
+Function GoHub {
   Param(
-    [String] $Language = "python",
-    [String] $Base     = $script:DEFAULT_BASE
+    [Parameter(Mandatory=$true)]
+    [String] $project,
+    [String] $user = "kfsone",
+    [Switch] $create
   )
 
-  $destination = Join-Path -Path "$Base" -ChildPath "$Language"
-  if ($pscmdlet.ShouldProcess($destination, "Change Directory")) {
-    Set-Location $destination
+  $userPath = Join-Path $env:GITHUB_PATH $user
+  if (-Not(Test-Path $userPath)) {
+    if (!$create) {
+      Write-Error "No such user folder: $userPath"
+      return 1
+    }
+    New-Item -Directory $userPath -ErrorAction Stop
   }
+
+  $projectPath = Join-Path $userPath $project
+  if (-Not(Test-Path $projectPath)) {
+    if (!$create) {
+      Write-Error "No such project folder: $projectPath"
+      return 1
+    }
+    New-Item -Directory $projectPath -ErrorAction stop
+    git init .
+  }
+
+  Set-Location $projectPath
+}
+
+Function Tinker {
+    Param([String] $language = "python")
+    Set-Location (Join-Path $env:TINKER_PATH $language)
 }
