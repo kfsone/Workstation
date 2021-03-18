8fc2fa9616092a6901b7217b48d2e9b08eb63e4e
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-03-18 10:44:53 -0700

    better go path prompting, still not happy with it

--- etc/Powershell.rc.d/Prompt.rc.ps1
+++ etc/Powershell.rc.d/Prompt.rc.ps1
@@ -1,10 +1,31 @@
+Function GetGitRev () {
+	if (git rev-parse --git-dir 2> $null) {
+		$symbolicref = $(git symbolic-ref --short HEAD 2>$NULL)
+		if (!$symbolicref) {
+				$symbolicref = $(git describe --tags --always 2>$NULL)
+		}
+    return $symbolicref
+	}
+
+  return ""
+}
+
 ###############################################################################
 # Change prompt to be two lines.
 #
 Function Prompt {
   $location = "[$($executionContext.SessionState.Path.CurrentLocation)]"
-  Write-Host -Background 1 -Foreground 7 "${location}`r"
-  "$('>' * ($nestedPromptLevel + 1)) "
+  $branch = GetGitRev
+  if ($branch) { 
+    if ($branch -eq 'master') {
+      $branchtext = "`e[44m:${branch}:`e[0m "
+    } else {
+      $branchtext = "`e[45m|${branch}|`e[0m "
+    }
+  }
+  $promptlevel = '>' * ($nestedPromptLevel + 1)
+  "${branchtext}${location}`n${promptlevel} "
   # .Link# http://go.microsoft.com/fwlink/?LinkID=225750
   # # .ExternalHelp System.Management.Automation.dll-help.xml
 }
+
