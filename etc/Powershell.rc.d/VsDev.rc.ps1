2ac348a327caeacf6dd52e208a9726aa6fc9ce71
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-04-19 12:07:56 -0700

    vsdev

---
+++ etc/Powershell.rc.d/VsDev.rc.ps1
@@ -0,0 +1,11 @@
+$VsWhere = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio' Installer vswhere.exe
+if ( Test-Path $VsWhere )
+{
+  $VisualStudioInstallPath = & $VsWhere -prerelease -latest -property installationPath
+  Import-Module (Join-Path $VisualStudioInstallPath Common7 Tools Microsoft.VisualStudio.DevShell.dll)
+
+  function vsdev () {
+    Enter-VsDevShell -VsInstallPath $VisualStudioInstallPath -SkipAutomaticLocation -DevCmdArguments "-arch=amd64 -host_arch=amd64"
+  }
+}
+
