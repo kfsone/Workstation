1a7cec0d1f462b311cbfb7582af94de9189014b3
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-07-04 18:02:05 -0700

    compatability

--- etc/Powershell.rc.d/Github.rc.ps1
+++ etc/Powershell.rc.d/Github.rc.ps1
@@ -13,13 +13,13 @@ if (-not $env:GITHUB_ROOT) {
   }
 }
 if ($env:GITHUB_ROOT) {
-  $env:GITHUB_CD = Join-Path $env:GITHUB_ROOT "github.com" "kfsone"
+  $env:GITHUB_CD = Join-Path (Join-Path $env:GITHUB_ROOT "github.com") "kfsone"
 }
 
 Function ghcd {
   Param([string] $Project = $env:GITHUB_CD)
 
-  if ($Project -NE $env:GITHUB_ROOT && $Project -NE $env:GITHUB_CD) {
+  if ($Project -NE $env:GITHUB_ROOT -And $Project -NE $env:GITHUB_CD) {
     Set-Location -erroraction stop $env:GITHUB_ROOT
     $candidates = "nibble.home.kfs.org","nibble.home.kfs.org/oliver","nibble.home.kfs.org","github.com/kfsone","github.com"
     foreach ($c in $candidates) {
--- etc/Powershell.rc.d/VsDev.rc.ps1
+++ etc/Powershell.rc.d/VsDev.rc.ps1
@@ -1,8 +1,8 @@
-$VsWhere = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio' Installer vswhere.exe
+$VsWhere = Join-Path (Join-Path (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio') Installer) vswhere.exe
 if ( Test-Path $VsWhere )
 {
   $VisualStudioInstallPath = & $VsWhere -prerelease -latest -property installationPath
-  Import-Module (Join-Path $VisualStudioInstallPath Common7 Tools Microsoft.VisualStudio.DevShell.dll)
+  Import-Module (Join-Path (Join-Path (Join-Path $VisualStudioInstallPath Common7) Tools) Microsoft.VisualStudio.DevShell.dll)
 
   function vsdev () {
     Enter-VsDevShell -VsInstallPath $VisualStudioInstallPath -SkipAutomaticLocation -DevCmdArguments "-arch=amd64 -host_arch=amd64"
