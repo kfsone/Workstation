aabe902ea7b22f3be85db75bcd07b07c1a5089d3
Oliver Smith <oliver@kfs.org>
2022-02-01 12:56:34 -0800

    Remove unused aliases

--- etc/Powershell.rc.d/Aliases.rc.ps1
+++ etc/Powershell.rc.d/Aliases.rc.ps1
@@ -1,7 +1,4 @@
 # Aliases for things that are missing on Windows
 if ($IsWindows) {
   Set-Alias   vi      vim
-  Set-Alias   which   Get-Command
-  Set-Alias   grep    Grep-String
-  Set-Alias   touch   Touch-Item
 }
