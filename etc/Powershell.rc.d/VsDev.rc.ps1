e1d90ec150be2ca2282ce08ce3901c9c987cdefc
Oliver Smith <oliver@kfs.org>
2022-04-20 01:31:10 -0700

    Comments and cleanup on vsdev

--- etc/Powershell.rc.d/VsDev.rc.ps1
+++ etc/Powershell.rc.d/VsDev.rc.ps1
@@ -1,11 +1,27 @@
-function vsdev () {
+<#
+.Description
+Adds the 'vsdev' (alias for Enable-VsDevShell) command to your powershell arsenal.
+#>
+
+
+function Enable-VsDevShell () {
+  # Locate the visual studio installer's "where" tool for locating vs paths.
   $VsWhere = Join-Path (Join-Path (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio') Installer) vswhere.exe
-  if ( Test-Path $VsWhere ) {
-    $VisualStudioInstallPath = & $VsWhere -prerelease -latest -property installationPath | select -last 1
-    Import-Module -ea stop (Join-Path (Join-Path (Join-Path $VisualStudioInstallPath Common7) Tools) Microsoft.VisualStudio.DevShell.dll)
-    Enter-VsDevShell -VsInstallPath $VisualStudioInstallPath -SkipAutomaticLocation -DevCmdArguments "-arch=amd64 -host_arch=amd64"
-  } else {
-    write-error "Could not find ${vswhere}"
+  if ( ! (Test-Path $VsWhere) ) {
+    # Not found, can't help.
+    throw "Visual studio is not installed in a recognized location. Could not find ${vswhere}"
   }
+  
+  # Get the most recent install path (-last 1) from its list of installed locations.
+  $VisualStudioInstallPath = & $VsWhere -prerelease -latest -property installationPath | select -last 1
+
+  # Import the devshell module from that install
+  Import-Module -ea stop (Join-Path (Join-Path (Join-Path $VisualStudioInstallPath Common7) Tools) Microsoft.VisualStudio.DevShell.dll)
+
+  # Use the module's "Enter-VsDevShell" to enable it properly
+  Enter-VsDevShell -VsInstallPath $VisualStudioInstallPath -SkipAutomaticLocation -DevCmdArguments "-arch=amd64 -host_arch=amd64"
 }
+
+# Handy alias.
+New-Alias -Name vsdev -Value Enable-VisualStudioEnv
 
